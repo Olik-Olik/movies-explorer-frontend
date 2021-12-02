26cce5b5f073b2b8e5d437113473893ada53cbd3
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-12-02 18:36:30 +0300

    Aboutme

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import logo from './images/logo.svg';
+import image_world from './images/Image_world.svg'
+import './components/App.css';
+import Main from './components/Main';
+
+
+function App(props) {
+  return (
+      <>
+      <Main/>
+      </>
+  )}
+  export default App;
+    /*<div className="App">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <div className= "Description-header">
+          Учебный проект студента факультета Веб-разработки.
+        </div>
+      </header>
+        <img src={image_world} className="image_world" alt="непонятная картинка" />
+            <div className= "Description-content">
+        Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+            </div>
+
+
+    </div>
+  );
+}*/
+
+
