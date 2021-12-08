6fa2a8abe8248fea55bec51abcfdc02d21710ad6
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-12-08 23:42:04 +0300

    Tech_button

--- src/components/Techs.js
+++ src/components/Techs.js
@@ -7,16 +7,16 @@ function Techs(props){
     </p>
     <p className= "techs__title">7 технологий
     </p>
-    <p className= "techs__title techs__body">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте. </p>
+    <p className= "techs__body">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте. </p>
 
-    <div  className="techs__button">
-        <button className="techs__button_1 techs__button" onClick={'{/* не тыкабельна*/}'}>HTML</button>
-        <button className="techs__button_2 techs__button" onClick={'{/* не тыкабельна */}'}>CSS</button>
-        <button className="techs__button_3 techs__button" onClick={'{/* не тыкабельна */}'}>JS</button>
-        <button className="techs__button_4 techs__button" onClick={' не тыкабельна'}>React</button>
-        <button className="techs__button_5 techs__button" onClick={' не тыкабельна'}>Git</button>
-        <button className="techs__button_6 techs__button" onClick={' не тыкабельна '}>Express.js</button>
-        <button className="techs__button_7 techs__button" onClick={' не тыкабельна'}>mongoDB</button>
+    <div  className="tech__button">
+        <button className="techs__button" onClick={'{/* не тыкабельна*/}'}>HTML</button>
+        <button className="techs__button" onClick={'{/* не тыкабельна */}'}>CSS</button>
+        <button className=" techs__button" onClick={'{/* не тыкабельна */}'}>JS</button>
+        <button className=" techs__button" onClick={' не тыкабельна'}>React</button>
+        <button className="techs__button" onClick={' не тыкабельна'}>Git</button>
+        <button className="techs__button" onClick={' не тыкабельна '}>Express.js</button>
+        <button className=" techs__button" onClick={' не тыкабельна'}>mongoDB</button>
 
     </div>
 </section>)
