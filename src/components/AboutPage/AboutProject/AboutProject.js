f368817c90ec7e99939a4cd490a3fac500f1dec1
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-12-12 19:54:49 +0300

    Верстка О Проекте под все размеры

--- src/components/AboutPage/AboutProject/AboutProject.js
+++ src/components/AboutPage/AboutProject/AboutProject.js
@@ -6,25 +6,29 @@ import '../../../index.css';
 function AboutProject(props) {
     return (
         <section className="main">
-        <aboutProject className="aboutProject">
+        {/*<aboutProject className="aboutProject">*/}
             <div className="aboutProject__title">О проекте
             </div>
 
             <div className="aboutProject__all__stolb">
                 {/*титлы в 2-ух столбцах*/}
                 <div className="aboutProject__1stolb">
-                    <div className="aboutProject__title1stolb">Дипломный проект включал 5 этапов</div>
-                    <div className="aboutProject__title2stolb">На выполнение диплома ушло 5 недель</div>
+                    <p className="aboutProject__title1stolb">Дипломный проект включал 5 этапов</p>
+                    {/*  <div className="aboutProject__title2stolb">На выполнение диплома ушло 5 недель</div>*/}
+                    <p className="aboutProject__process1stolb">Составление плана, работу над бэкендом, вёрстку,
+                    добавление функциональности и финальные доработки.  </p>
                 </div>
 
+
                 <div className="aboutProject__2stolb">
-                    <div className="aboutProject__process1stolb">Составление плана, работу над бэкендом, вёрстку,
+                   {/* <div className="aboutProject__process1stolb">Составление плана, работу над бэкендом, вёрстку,
                         добавление функциональности и финальные доработки.
-                    </div>
-                    <div className="aboutProject__process2stolb">У каждого этапа был мягкий и жёсткий дедлайн,
-                        которые нужно было соблюдать, чтобы успешно защититься.
-                    </div>
+                    </div>*/}
+                    <p className="aboutProject__title2stolb">На выполнение диплома ушло 5 недель</p>
+                    <p className="aboutProject__process2stolb">У каждого этапа был мягкий и жёсткий дедлайн,
+                        которые нужно было соблюдать, чтобы успешно защититься. </p>
                 </div>
+
             </div>
             {/* Сами титлы 1 и 4 неделя*/}
                 <div  className="aboutProject__button">
@@ -37,10 +41,10 @@ function AboutProject(props) {
                              </div>
             {/* Надписи под 1 и 4 неделями*/}
             <div  className="aboutProject__button">
-            <p className="aboutProject__button_first_down" onClick={'{/* не тыкабельна зеленая */}'}>Front-end</p>
-            <p className="aboutProject__button_second_down" onClick={'{/* не тыкабельна серая*/}'}>Back-end</p>
+            <p className="aboutProject__button_first_down" onClick={'{/* не тыкабельна зеленая */}'}>Back-end</p>
+            <p className="aboutProject__button_second_down" onClick={'{/* не тыкабельна серая*/}'}>Front-end</p>
             </div>
-        </aboutProject>
+     {/*   </aboutProject>*/}
         </section>);
 }
 
