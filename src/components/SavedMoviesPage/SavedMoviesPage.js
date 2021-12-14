5d699da62dd14c09a5c4018944026970ab0ddcbb
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-12-14 22:08:17 +0300

    Search form is ready for small Size!!! without Короткометражка

--- src/components/SavedMoviesPage/SavedMoviesPage.js
+++ src/components/SavedMoviesPage/SavedMoviesPage.js
@@ -5,7 +5,7 @@ import Card from "../MoviesPage/Card/Card";
 import ResultMainSearch from "../MoviesPage/ResultMainSearch/ResultMainSearch";
 import MoviesCardList from "../MoviesPage/MoviesCardList/MoviesCardList";
 import '../../index.css';
-
+import HeaderSavedFilms from "../MoviesPage/HeaderSavedFilms/HeaderSavedFilms";
 import Footer from "../Footer";
 import Header from "../Header";
 
@@ -13,9 +13,10 @@ import Header from "../Header";
 function SavedMoviesPages(props){
     return (
         <>
+            <HeaderSavedFilms/>
             <ResultMainSearch/>
-            <Card/>
-      {/*      <MoviesCardList/>*/}
+            {/*<Card/>*/}
+            <MoviesCardList/>
             <ResultMainMore/>
             <Footer/>
         </>
