76d908b2e51d24da78b7faa25a2017371f4b34a8
Olik Sanina <Rinpoche.Despot@yandex.ru>
2022-01-14 19:44:47 +0300

    Найти проект по ссылке https://diplomback.nomoredomains.work/ README

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -133,8 +133,8 @@ class MainApi {
 
   }
    const apiAuth = new MainApi({
-  /*      address: BASE_URL,*/
-        address: "http://localhost:3627/api",
+        address: BASE_URL,
+      /*  address: "http://localhost:3627/api",*/
         headers: {'Content-Type': 'application/json'}
     });
 
